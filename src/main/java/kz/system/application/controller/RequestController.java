ba2bbc294f0d9dda8d5c21cae9168bcbcc732b48
AslanMalik <berd.bobby@mail.ru>
2026-03-10 10:19:47 +0500

    crm

--- src/main/java/kz/system/application/controller/RequestController.java
+++ src/main/java/kz/system/application/controller/RequestController.java
@@ -1,9 +1,13 @@
 package kz.system.application.controller;
 
 import kz.system.application.db.DBConnector;
+import kz.system.application.entity.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -19,9 +23,48 @@ public class RequestController {
         return "add-student-page";
     }
 
-//    @PostMapping(value = "/add-student")
-//    public String addStudent(Request request) {
-//        DBConnector.addStudent(request);
-//        return "redirect:/";
-//    }
+    @PostMapping(value = "/add-request")
+    public String addStudent(Request request) {
+        DBConnector.addStudent(request);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/new-request")
+    public String getNewRequest(Model model) {
+        model.addAttribute("requests", DBConnector.getAllStudents(true));
+        return "new";
+    }
+
+    @GetMapping(value = "/old-request")
+    public String getOldRequest(Model model) {
+        model.addAttribute("requests", DBConnector.getAllStudents(false));
+        return "old";
+    }
+
+    @GetMapping(value = "/details/{id}")
+    public String getStudent(@PathVariable int id, Model m) {
+        m.addAttribute("request", DBConnector.getStudentById(id));
+
+        if(DBConnector.getStudentById(id).isHandled()) {
+            return "details-checked";
+        }
+        return "details";
+    }
+
+    @PostMapping(value = "/handle-request")
+    public String HandleReq(@RequestParam int id) {
+        Request request = DBConnector.getStudentById(id);
+        request.setHandled(true);
+        DBConnector.updateStudent(request);
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/delete-request")
+    public String deleteReq(@RequestParam int id) {
+        DBConnector.deleteStudent(id);
+        return "redirect:/";
+    }
+
+
+
 }
--- src/main/java/kz/system/application/db/DBConnector.java
+++ src/main/java/kz/system/application/db/DBConnector.java
@@ -46,5 +46,103 @@ public class DBConnector {
         }
         return requests;
     }
+
+    public static ArrayList<Request> getAllStudents(boolean handled){
+        ArrayList<Request> requests = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM orda.requests WHERE handled = ?");
+            statement.setBoolean(1, handled);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Request request = new Request();
+                request.setId(resultSet.getInt("id"));
+                request.setUserName(resultSet.getString("name"));
+                request.setCourseName(resultSet.getString("course_name"));
+                request.setCommentary(resultSet.getString("commentary"));
+                request.setPhone(resultSet.getString("phone"));
+                request.setHandled(resultSet.getBoolean("handled"));
+
+                requests.add(request);
+            }
+            resultSet.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return requests;
+    }
+
+    public static Request getStudentById(int id) {
+        Request request = new Request();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM orda.requests WHERE id=?");
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                request.setId(resultSet.getInt("id"));
+                request.setUserName(resultSet.getString("name"));
+                request.setCourseName(resultSet.getString("course_name"));
+                request.setCommentary(resultSet.getString("commentary"));
+                request.setPhone(resultSet.getString("phone"));
+                request.setHandled(resultSet.getBoolean("handled"));
+
+            }
+
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return request;
+    }
+
+
+    public static void addStudent(Request request) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO orda.requests (name, course_name, commentary, phone, handled) VALUES (?, ?, ?, ?, ?)" );
+            statement.setString(1, request.getUserName());
+            statement.setString(2, request.getCourseName());
+            statement.setString(3, request.getCommentary());
+            statement.setString(4, request.getPhone());
+            statement.setBoolean(5, request.isHandled());
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteStudent(int id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM orda.requests WHERE id = ?");
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateStudent(Request request) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE orda.requests SET name = ?, course_name=?, commentary =?, phone=?, handled=? WHERE id = ? ");
+            statement.setString(1, request.getUserName());
+            statement.setString(2, request.getCourseName());
+            statement.setString(3, request.getCommentary());
+            statement.setString(4, request.getPhone());
+            statement.setBoolean(5, request.isHandled());
+            statement.setInt(6, request.getId());
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
